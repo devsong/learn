c1a09a568cfccfd2add1083997b609951b7897dc
guanzhisong <guanzhisong@gomeplus.com>
2017-01-24 16:07:41 +0800

    add spring boot support

--- src/main/java/com/gzs/learn/gc/GcDemo.java
+++ src/main/java/com/gzs/learn/gc/GcDemo.java
@@ -1,32 +1,26 @@
 package com.gzs.learn.gc;
 
-import java.util.concurrent.CountDownLatch;
-
 public class GcDemo {
 	public static byte[] s_arr = new byte[1024 * 1024 * 2];
 
 	public static void main(String[] args) throws InterruptedException {
-		/*byte[] b = null;
-		for (int i = 0; i < 10; i++) {
-			byte[] arr = new byte[1024 * 128];
-			System.out.println(i);
-			b=arr;
-			System.gc();
-		}
-		System.out.println(b.length);
-		
-		CountDownLatch latch = new CountDownLatch(1);
-		latch.await();*/
-		
+		/*
+		 * byte[] b = null; for (int i = 0; i < 10; i++) { byte[] arr = new
+		 * byte[1024 * 128]; System.out.println(i); b=arr; System.gc(); }
+		 * System.out.println(b.length);
+		 * 
+		 * CountDownLatch latch = new CountDownLatch(1); latch.await();
+		 */
+
 		StringBuffer sb1 = new StringBuffer("a");
-		
+
 		StringBuffer sb2 = new StringBuffer("a");
-		
+
 		System.out.println(sb1.toString().equals(sb2.toString()));
-		
-		Byte a = new Byte((byte)3);
-		Byte b = new Byte((byte)3);
-		
+
+		Byte a = new Byte((byte) 3);
+		Byte b = new Byte((byte) 3);
+
 		System.out.println(a.equals(b));
 	}
 }
