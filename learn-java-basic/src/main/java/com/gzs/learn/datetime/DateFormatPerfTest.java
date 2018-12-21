160549d8f81f568f6d50fbdc8d7e04d2530fc740
guanzhisong <guanzhisong@gmail.com>
2018-12-21 19:19:57 +0800

    add timer

--- learn-java-basic/src/main/java/com/gzs/learn/datetime/DateFormatPerfTest.java
+++ learn-java-basic/src/main/java/com/gzs/learn/datetime/DateFormatPerfTest.java
@@ -14,84 +14,97 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class DateFormatPerfTest {
-    private String pattern = "yyyy-MM-dd HH:mm:ss";
-    private int threadNum = 10;
-    private int loopTimes = 1000000;
-    private final ThreadLocal<SimpleDateFormat> sdfLocal =
-            ThreadLocal.withInitial(() -> new SimpleDateFormat(pattern));
-    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+	private String pattern = "yyyy-MM-dd HH:mm:ss";
+	private int threadNum = 10;
+	private int loopTimes = 1000000;
+	private final ThreadLocal<SimpleDateFormat> sdfLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat(pattern));
+	private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    @Test
-    public void testSdfLocal() throws InterruptedException {
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        final CountDownLatch latch = new CountDownLatch(threadNum);
-        for (int i = 0; i < threadNum; i++) {
-            executorService.submit(() -> {
-                for (int j = 0; j < loopTimes; j++) {
-                    SimpleDateFormat sdf = sdfLocal.get();
-                    try {
-                        sdf.parse("2010-10-01 00:00:10");
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                }
-                latch.countDown();
-            });
+	@Test
+	public void testSdfLocal() throws InterruptedException {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		final CountDownLatch latch = new CountDownLatch(threadNum);
+		for (int i = 0; i < threadNum; i++) {
+			executorService.submit(() -> {
+				for (int j = 0; j < loopTimes; j++) {
+					SimpleDateFormat sdf = sdfLocal.get();
+					try {
+						sdf.parse("2010-10-01 00:00:10");
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
+				latch.countDown();
+			});
 
-        }
-        latch.await();
-        stopwatch.stop();
-        log.info("sdf local cost:" + stopwatch.toString());
-    }
+		}
+		latch.await();
+		stopwatch.stop();
+		log.info("sdf local cost:" + stopwatch.toString());
+	}
 
+	@Test
+	public void testSdf() throws InterruptedException {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		final CountDownLatch latch = new CountDownLatch(threadNum);
+		for (int i = 0; i < threadNum; i++) {
+			executorService.submit(() -> {
+				for (int j = 0; j < loopTimes; j++) {
+					SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+					try {
+						sdf.parse("2010-10-01 00:00:10");
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
+				latch.countDown();
+			});
 
-    @Test
-    public void testSdf() throws InterruptedException {
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        final CountDownLatch latch = new CountDownLatch(threadNum);
-        for (int i = 0; i < threadNum; i++) {
-            executorService.submit(() -> {
-                for (int j = 0; j < loopTimes; j++) {
-                    SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-                    try {
-                        sdf.parse("2010-10-01 00:00:10");
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                }
-                latch.countDown();
-            });
+		}
+		latch.await();
+		stopwatch.stop();
+		log.info("sdf cost:" + stopwatch.toString());
+	}
 
-        }
-        latch.await();
-        stopwatch.stop();
-        log.info("sdf cost:" + stopwatch.toString());
-    }
+	@Test
+	public void testPojo() throws InterruptedException {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		final CountDownLatch latch = new CountDownLatch(threadNum);
+		for (int i = 0; i < threadNum; i++) {
+			executorService.submit(() -> {
+				for (int j = 0; j < loopTimes; j++) {
+					new Pojo();
+					// new Object();
+				}
+				latch.countDown();
+			});
 
-    @Test
-    public void testPojo() throws InterruptedException {
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        final CountDownLatch latch = new CountDownLatch(threadNum);
-        for (int i = 0; i < threadNum; i++) {
-            executorService.submit(() -> {
-                for (int j = 0; j < loopTimes; j++) {
-                    new Pojo();
-                    // new Object();
-                }
-                latch.countDown();
-            });
+		}
+		latch.await();
+		stopwatch.stop();
+		log.info("pojo cost:" + stopwatch.toString());
+	}
 
-        }
-        latch.await();
-        stopwatch.stop();
-        log.info("pojo cost:" + stopwatch.toString());
-    }
-
+	@Test
+	public void testSdfObject() throws InterruptedException {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		final CountDownLatch latch = new CountDownLatch(threadNum);
+		for (int i = 0; i < threadNum; i++) {
+			executorService.submit(() -> {
+				for (int j = 0; j < loopTimes; j++) {
+					new SimpleDateFormat();
+				}
+				latch.countDown();
+			});
+		}
+		latch.await();
+		stopwatch.stop();
+		log.info(" cost:" + stopwatch.toString());
+	}
 }
 
-
 class Pojo {
-    int id = 0;
-    String name = "123";
-    Object object = new Object();
+	int id = 0;
+	String name = "123";
+	Object object = new Object();
 }
