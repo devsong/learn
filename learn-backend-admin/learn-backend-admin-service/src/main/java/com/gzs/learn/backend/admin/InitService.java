292b63c748dfa9a9d7dc91dbccb5cacfd3b7820f
guanzhisong <guanzhisong@gmail.com>
2019-05-17 18:53:10 +0800

    feat:modify code

--- learn-backend-admin/learn-backend-admin-service/src/main/java/com/gzs/learn/backend/admin/InitService.java
+++ learn-backend-admin/learn-backend-admin-service/src/main/java/com/gzs/learn/backend/admin/InitService.java
@@ -4,6 +4,9 @@ import javax.annotation.PostConstruct;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +24,13 @@ public class InitService {
         log.info("init service start-------->");
         SecurityUtils.setSecurityManager(SpringContextUtil.getBean(DefaultWebSecurityManager.class));
         log.info("init service end-------->");
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.aop.auto", havingValue = "false")
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
     }
 }
