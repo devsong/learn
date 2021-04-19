ebfd7ee92ff29df7c8b755776012880aa7fe6333
guanzhisong <guanzhisong@gmail.com>
2021-04-19 14:21:42 +0800

    feat:add array example

--- learn-java-basic/src/main/java/com/gzs/learn/algorithm/array/ArrayExample.java
+++ learn-java-basic/src/main/java/com/gzs/learn/algorithm/array/ArrayExample.java
@@ -3,27 +3,43 @@ package com.gzs.learn.algorithm.array;
 import org.junit.Test;
 
 public class ArrayExample {
-    int[] sortArr = {1, 2, 2, 3, 3, 4, 4, 4, 4, 4, 5};
+    int[] sortArr = {1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 4, 5,5};
 
     @Test
     public void testRemoveRepeat() {
         int count = this.removeRepeat();
         System.out.println("count:" + count);
-        this.printArr(sortArr, count);
+        // this.printArr(sortArr, sortArr.length);
     }
 
     public int removeRepeat() {
-        int insertIndex = 0;
-        int repeat = sortArr[insertIndex];
-        for (int i = 0; i < sortArr.length; i++) {
-            if (repeat == sortArr[i]) {
-                continue;
+        int repeatCount = 0;
+        int totalRepeatCount = 0;
+        int repeat = sortArr[0];
+        boolean flag = false;
+        for (int i = 1; i < sortArr.length - repeatCount + 1; i++) {
+            if (i >= sortArr.length) {
+                break;
             }
-            repeat = sortArr[i];
-            this.swap(sortArr, insertIndex, i + 1);
-            insertIndex++;
+            if (sortArr[i] == repeat) {
+                repeatCount++;
+                totalRepeatCount++;
+                flag = true;
+            } else {
+                repeat = sortArr[i];
+                flag = false;
+            }
+            if (repeatCount > 0 && !flag) {
+                for (int n = i - 1; n <= sortArr.length - repeatCount; n++) {
+                    swap(sortArr, n - repeatCount, n);
+                }
+                this.printArr(sortArr, sortArr.length);
+                System.out.println();
+                i -= repeatCount;
+                repeatCount = 0;
+            }
         }
-        return insertIndex;
+        return sortArr.length - totalRepeatCount;
     }
 
     /**
